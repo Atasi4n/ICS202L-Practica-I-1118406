ff34cb469b3dfedbcfd47f24742ec447241d7f89
root <root@DESKTOP-CDE3Q7E.>
2024-08-20 19:49:15 -0400

    Seccion 0x250

--- firstprog.c
+++ firstprog.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-int main()
-{
- int i;
- for (i=0; i <10; i++)
+ int main()
  {
-  puts("Hello, world!\n");
+ int i;
+ for(i=0; i < 10; i++)       
+{
+ // Loop 10 times.
+  puts("Hello, world!\n");  // put the string to the output.
  }
- return 0;
+ return 0;                   
 }
